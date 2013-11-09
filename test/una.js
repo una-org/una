2842fed28053483c2040ba5195187dadb007bd06
soedar <soedar.sg@gmail.com>
2013-11-10 00:46:31 +0800

    Added socket code and some test cases

--- test/una.js
+++ test/una.js
@@ -1,18 +1,92 @@
 var express = require('express');
-var una = require('..');
+var una_js = require('..');
 var should = require('should');
 var http = require('http');
+var request = require('supertest');
+var ioc = require('socket.io-client');
+
+var start_una = function() {
+    var una = una_js();
+    var server = http.createServer().listen(3000);
+    una.listen(server);
+    return server;
+}
+
+var new_socket = function() {
+    return ioc.connect('http://localhost:3000', {'force new connection': true});
+}
+
+var server = start_una();
 
 describe('una', function() {
     describe('running of server', function() {
-        it('should run server', function(done) {
-            var una_app = una();
-            var app = una_app.app;
-            app.set('port', process.env.PORT || 3216);
+        var socket;
 
-            una_app.listen(http.createServer, app.get('port'), function() {
+        beforeEach(function(done) {
+            socket = new_socket();
+            socket.on('connect', function() {
                 done();
+            })
+        });
+        afterEach(function(done) {
+            if (socket.socket.connected) {
+                socket.disconnect();
+            }
+            done();
+        });
+
+        it('should receive MOTD', function(done) {
+            socket.on('server-message', function(data) {
+                if (data.message.indexOf('MOTD') !== -1) {
+                    done();
+                }
             });
         });
     });
+
+    describe('screen', function() {
+        var socket;
+
+        beforeEach(function(done) {
+            socket = new_socket();
+            socket.on('connect', function() {
+                done();
+            })
+        });
+        afterEach(function(done) {
+            if (socket.socket.connected) {
+                socket.disconnect();
+            }
+            done();
+        });
+
+        it('should be able to register', function(done) {
+            socket.emit('register-screen', {room: '123'});
+            socket.on('screen-ready', function(data) {
+                if (data.success) {
+                    done();
+                }
+            });
+        });
+
+        it('should only have one screen', function(done) {
+            var room_data = {room: '123'};
+            socket.emit('register-screen', room_data);
+
+            socket.on('screen-ready', function(data) {
+                if (data.success) {
+                    // We now have one screen, we try make another screen
+                    // join the same room
+                    var s2 = new_socket();
+                    s2.emit('register-screen', room_data);
+                    s2.on('screen-ready', function(data) {
+                        if (!data.success) 
+                            done();
+                    });
+                }
+            });
+        });
+
+        
+    })
 });
