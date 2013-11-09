212e3b133edca5b218a4398cdbd8aa52531acedd
soedar <soedar.sg@gmail.com>
2013-11-10 03:52:20 +0800

    Merged all the una client files together

--- client/una.js
+++ client/una.js
@@ -50,3 +50,55 @@ function rttHeartBeat() {
 }
 
 setInterval(rttHeartBeat, INTERVAL_HEART_BEAT);
+
+
+var UnaController = (function() {
+    var register = function(room_id, user_data, callback) {
+        socket.emit('register-controller', {room: room_id, user_data: user_data});
+
+        socket.on('controller-ready', function(data) {
+            callback(data);
+        });
+    }
+
+    var sendInput = function(user_data) {
+        socket.emit('controller-input', user_data);
+    }
+
+    return {register: register, 
+            sendInput: sendInput};
+})();
+
+var UnaScreen = (function() {
+    var register = function(room_id, user_data, callback) {
+        socket.emit('register-screen', {room: room_id, user_data: user_data});
+
+        socket.on('screen_ready', function(data) {
+            callback(data);
+        });
+    }
+
+    var onControllerJoin = function(callback) {
+        socket.on('controller-join', function(data) {
+            var success = callback(data);
+            socket.emit('acknowledge-controller', {controller_id: data.una.id, success: success});
+        });
+    }
+
+    var onControllerLeave = function(callback) {
+        socket.on('controller-leave', function(data) {
+            callback(data);
+        });
+    }
+
+    var onControllerInput = function(callback) {
+        socket.on('controller-input', function(data) {
+            callback(data);
+        });
+    }
+
+    return {register: register, 
+        onControllerJoin: onControllerJoin, 
+        onControllerLeave: onControllerLeave, 
+        onControllerInput: onControllerInput};
+})();
