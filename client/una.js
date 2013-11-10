1b7054fb4fbe83b53a85c4ba07e0b77973e66b11
soedar <soedar.sg@gmail.com>
2013-11-10 12:59:10 +0800

    Modified client una file

--- client/una.js
+++ client/una.js
@@ -30,6 +30,8 @@ var UnaController = (function() {
 })();
 
 var UnaScreen = (function() {
+    var controllerList = [];
+
     var register = function(room_id, user_data, callback) {
         socket.emit('register-screen', {room: room_id, user_data: user_data});
 
@@ -38,15 +40,20 @@ var UnaScreen = (function() {
         });
     }
 
+    // This method should only be called once
     var onControllerJoin = function(callback) {
         socket.on('controller-join', function(data) {
+            controllerList.push(data.una.id);
             var success = callback(data);
             socket.emit('acknowledge-controller', {controller_id: data.una.id, success: success});
         });
     }
 
+    // This method should only be called once
     var onControllerLeave = function(callback) {
         socket.on('controller-leave', function(data) {
+            var index = controllerList.indexOf(data.una.id);
+            controllerList.splice(index, 1);
             callback(data);
         });
     }
@@ -59,20 +66,10 @@ var UnaScreen = (function() {
 
     var sendInput = function(controller_id, user_data) {
         // Check if the controller we are sending to exists
-        if (controllerList.indexOf(controller_id)) {
+        if (controllerList.indexOf(controller_id) > -1) {
             socket.emit('screen-input', controller_id, user_data);
         }
     }
-
-    var controllerList = [];
-    onControllerJoin(function(data) {
-        controllerList.push(data.una.id);
-    });
-
-    onControllerLeave(function(data) {
-        var index = controllerList.indexOf(data.una.id);
-        controllerList.splice(index, 1);
-    })
 
     return {register: register, 
         onControllerJoin: onControllerJoin, 
